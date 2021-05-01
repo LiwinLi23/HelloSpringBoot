58bde463b3cfb50b700f152f9e15c17df79f8a56
lingke <litao@xiaolingent.cn>
2021-05-01 15:56:16 +0800

    Use YML to express obj.

---
+++ src/main/java/com/example/liwinli/HelloSpringBoot/com/example/liwinli/bean/Person.java
@@ -0,0 +1,90 @@
+package com.example.liwinli.HelloSpringBoot.com.example.liwinli.bean;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+@Component
+@ConfigurationProperties(prefix = "person")
+public class Person {
+    private String lastName;
+    private Integer age;
+    private boolean isBoss;
+    private Date birth;
+
+    private Map<String, Object> map;
+    private List<Object> list;
+    private Dog dog;
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Boolean getBoss() {
+        return isBoss;
+    }
+
+    public void setBoss(Boolean boss) {
+        isBoss = boss;
+    }
+
+    public Date getBirth() {
+        return birth;
+    }
+
+    public void setBirth(Date birth) {
+        this.birth = birth;
+    }
+
+    public Map<String, Object> getMaps() {
+        return map;
+    }
+
+    public void setMaps(Map<String, Object> maps) {
+        this.map = maps;
+    }
+
+    public List<Object> getList() {
+        return list;
+    }
+
+    public void setList(List<Object> list) {
+        this.list = list;
+    }
+
+    public Dog getDog() {
+        return dog;
+    }
+
+    public void setDog(Dog dog) {
+        this.dog = dog;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "lastName='" + lastName + '\'' +
+                ", age=" + age +
+                ", isBoss=" + isBoss +
+                ", birth=" + birth +
+                ", maps=" + map +
+                ", list=" + list +
+                ", dog=" + dog +
+                '}';
+    }
+}
--- src/test/java/com/example/liwinli/HelloSpringBoot/HelloSpringBootApplicationTests.java
+++ src/test/java/com/example/liwinli/HelloSpringBoot/HelloSpringBootApplicationTests.java
@@ -1,13 +1,21 @@
 package com.example.liwinli.HelloSpringBoot;
 
+import com.example.liwinli.HelloSpringBoot.com.example.liwinli.bean.Person;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class HelloSpringBootApplicationTests {
+	@Autowired
+	Person person;
 
 	@Test
 	void contextLoads() {
+		System.out.print(person);
 	}
 
 }
