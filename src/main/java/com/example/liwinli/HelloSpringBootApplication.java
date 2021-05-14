c87146aa817fb6d3a977418e59b8fe75cbd0df14
lingke <litao@xiaolingent.cn>
2021-05-14 19:35:00 +0800

    Add comment for HelloController.

--- src/main/java/com/example/liwinli/HelloSpringBootApplication.java
+++ src/main/java/com/example/liwinli/HelloSpringBootApplication.java
@@ -3,10 +3,14 @@ package com.example.liwinli;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @SpringBootApplication表明这是一个SpringBoot运用
+ */
 @SpringBootApplication
 public class HelloSpringBootApplication {
 
 	public static void main(String[] args) {
+		// SpringApp启动起来
 		SpringApplication.run(HelloSpringBootApplication.class, args);
 	}
 
---
+++ src/main/java/com/example/liwinli/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.example.liwinli.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+    @ResponseBody
+    @RequestMapping(value = "/hello")
+    public String sayHello() { return "Hello, world!"; }
+}
