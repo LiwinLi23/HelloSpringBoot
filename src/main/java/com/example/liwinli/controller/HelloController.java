154f5f293a13cbcb96ab6aa76655335350a65b5f
lingke <litao@xiaolingent.cn>
2021-05-25 19:16:29 +0800

    Update HelloController.

--- src/main/java/com/example/liwinli/controller/HelloController.java
+++ src/main/java/com/example/liwinli/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.liwinli.controller;
 
+import com.example.liwinli.pojo.vo.DemoUserVO;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -9,4 +10,12 @@ public class HelloController {
     @ResponseBody
     @RequestMapping(value = "/hello")
     public String sayHello() { return "Hello, world!"; }
+
+    @ResponseBody
+    @RequestMapping(value = "/users")
+    public DemoUserVO getDemoUserVo() {
+        DemoUserVO vo = new DemoUserVO();
+        vo.setUser_name("Liwin Lee"); vo.setPassword("lt52310431");
+        return vo;
+    }
 }
--- src/test/java/com/example/liwinli/HelloSpringBootApplicationTests.java
+++ src/test/java/com/example/liwinli/HelloSpringBootApplicationTests.java
@@ -1,6 +1,7 @@
 package com.example.liwinli;
 
 import com.example.liwinli.com.example.liwinli.bean.Person;
+import com.example.liwinli.controller.HelloController;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
@@ -8,7 +9,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -36,5 +42,13 @@ class HelloSpringBootApplicationTests {
 	void testSlf4j() {
 		logger.trace("This is trace log"); logger.debug("This is debug log");
 		logger.info("This is info log"); logger.warn("This is warn log"); logger.error("This is error log");
+	}
+
+	private MockMvc mockMvc;
+
+	@Test
+	public void testHello() throws Exception {
+		MockMvcBuilders.standaloneSetup(new HelloController()).build().perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk());
 	}
 }
